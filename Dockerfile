7e181e0dd0b36593f2a158d367b514df270297a0
Matthew DeGuzman <91019033+matt-dz@users.noreply.github.com>
2025-02-13 19:04:17 -0500

    Add OAuth client metadata to project (#25)
    * fix the name of oauth client metadata file
    
    * add client-metadata.json to build process
    
    * remove quote from .env.example

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ RUN CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o bin/boshi-backend cmd/main
 FROM alpine:latest
 WORKDIR /app
 COPY --from=build /app/bin/boshi-backend /app/boshi-backend
+COPY --from=build /app/client-metadata.json /srv/client-metadata.json
 ENV PORT=80
+ENV SRV_DIR=/srv
 EXPOSE 80
 ENTRYPOINT ["/app/boshi-backend"]
--- internal/endpoints/oauth.go
+++ internal/endpoints/oauth.go
@@ -2,8 +2,16 @@ package endpoints
 
 import (
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func ServeOauthMetadata(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "/srv/client-metadata.json")
+	srvDir := os.Getenv("SRV_DIR")
+	if srvDir == "" {
+		bLogger.Error("SRV_DIR environment variable is not set")
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+	http.ServeFile(w, r, filepath.Join(srvDir, "client-metadata.json"))
 }
