933144baa5d21bb131dff9e75fb1fb1a6be9f504
Matthew DeGuzman <91019033+matt-dz@users.noreply.github.com>
2025-02-15 20:45:49 -0500

    Create separate routes for api and oauth (#29)
    * add separate endpoints for api and oauth
    
    * add separate ingress rule for oauth route
    
    * update subdomain for client-metadata
    
    * remove deployment script

--- backend/scripts/generate-subdomain.sh
+++ backend/scripts/generate-subdomain.sh
@@ -3,7 +3,7 @@
 branch=$(git branch --show-current)
 
 if [[ $branch == "main" ]]; then
-	echo "api-boshi"
+	echo "boshi"
 else
-	echo "$(git branch --show-current | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g' | sed 's/--*/-/g' | sed -E 's/^-+|-+$$//g')-api-boshi"
+	echo "$(git branch --show-current | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9]/-/g' | sed 's/--*/-/g' | sed -E 's/^-+|-+$$//g')-boshi"
 fi
