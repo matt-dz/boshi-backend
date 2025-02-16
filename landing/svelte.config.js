f25e6255a254e962d2abfcae6a5f8f2fd4ef9424
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-02-15 23:25:52 -0500

    install node-adapter and add build files

--- landing/svelte.config.js
+++ landing/svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import autoAdapter from '@sveltejs/adapter-auto';
+import nodeAdapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const isNode = process.env.ADAPTER === 'node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +14,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: isNode ? nodeAdapter({ out: 'build' }) : autoAdapter()
 	},
 	optimizeDeps: {
 		exclude: ['@icons-pack/svelte-simple-icons']
