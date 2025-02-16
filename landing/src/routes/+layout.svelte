bb810541d1311a6fa7c0182136b9f72b771142f7
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2025-02-16 11:04:24 -0500

    add VITE_BASE_URL as env variable to make metadata change with branch previews

--- landing/src/routes/+layout.svelte
+++ landing/src/routes/+layout.svelte
@@ -1,6 +1,29 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	const description = 'Join the only university-exclusive social media built on the at protocol.';
 </script>
 
+<svelte:head>
+	<!-- SEO Meta Tags -->
+	<title>Boshi</title>
+	<meta name="author" content="Matthew DeGuzman, Alex Eum" />
+
+	<!-- Open Graph/Facebook -->
+	<meta name="description" content={description} />
+	<meta property="og:title" content="Boshi" />
+	<meta property="og:description" content={description} />
+	<meta
+		property="og:image"
+		content={`https://${import.meta.env.VITE_BASE_URL}.deguzman.cloud/og-image.png`}
+	/>
+
+	<meta property="og:url" content={`https://${import.meta.env.VITE_BASE_URL}.deguzman.cloud`} />
+	<meta property="og:type" content="website" />
+
+	<!-- Twitter Card -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@matthew_d13" />
+</svelte:head>
 {@render children()}
