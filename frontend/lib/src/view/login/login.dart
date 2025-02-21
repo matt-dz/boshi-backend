81b88a15fb845ae7f38753c2da42e89f7d30b000
Alex Eum <aeum1003@gmail.com>
2025-02-21 12:56:18 -0500

    Form for login identifier

--- frontend/lib/src/view/login/login.dart
+++ frontend/lib/src/view/login/login.dart
@@ -1,31 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
+import 'package:provider/provider.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'package:frontend/src/model/oauth/oauth_repository.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<ShadFormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: Center(child: Text('Login')),
-      floatingActionButton: Consumer<OAuthRepository>(
-        builder: (context, oauth, child) {
-          return FilledButton(
-            onPressed: () async {
-              Uri authUri = await oauth.getAuthorizationURI("");
-              if (!await launchUrl(authUri)) {
-                throw Exception('Could not launch $authUri');
-              }
-            },
-            child: const Text("Login"),
-          );
-        },
-      ),
+      body: Center(
+          child: ShadCard(
+              width: 350,
+              title: const Text('Sign In'),
+              description:
+                  const Text('Use your identity to sign in with OAuth'),
+              child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: ShadForm(
+                      key: formKey,
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            ShadInputFormField(
+                                id: 'login_input_form',
+                                label: const Text("Identity"),
+                                placeholder: const Text('Enter your identity'),
+                                description: const Text(
+                                    "Login with any identifier on the atprotocol"),
+                                validator: (v) {
+                                  if (v.isEmpty) {
+                                    return 'Identifier must not be empty';
+                                  }
+                                  return null;
+                                }),
+                            const SizedBox(height: 16),
+                            Consumer<OAuthRepository>(
+                              builder: (context, oauth, child) {
+                                return ShadButton(
+                                  child: const Text("Sign in"),
+                                  onPressed: () async {
+                                    if (formKey.currentState!
+                                        .saveAndValidate()) {
+                                      Uri authUri = await oauth
+                                          .getAuthorizationURI(formKey
+                                              .currentState!
+                                              .value["login_input_form"]);
+                                      if (!await launchUrl(authUri)) {
+                                        throw Exception(
+                                            'Could not launch $authUri');
+                                      }
+                                    }
+                                  },
+                                );
+                              },
+                            )
+                          ]))))),
     );
   }
 }
